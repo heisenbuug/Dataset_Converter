8d01378400189c9fe24122f4c6c5eb14d1a0e955
Gopi M Tatiraju <deathcoderx@gmail.com>
2020-08-03 22:46:29 +0530

    Update csv_xml.cpp

--- csv_xml.cpp
+++ csv_xml.cpp
@@ -14,56 +14,59 @@ using namespace boost;
 
 class Convert
 {
-private:
-	static int ctr;
-	static std::unordered_map<std::string, int> fileNames;
-
-	std::vector<std::string> tokenize(std::string line)
+	void csvxmlHelper(std::string path)
 	{
-		std::vector<std::string> col_names;
-
-		tokenizer<escaped_list_separator<char> > tk(line, escaped_list_separator<char>());
-		for (tokenizer<escaped_list_separator<char> >::iterator i(tk.begin()); i != tk.end(); ++i)
-			col_names.push_back(*i);
-
-		return col_names;
-	}
-
-	void create_XML(std::vector<std::string>& tags, std::vector<std::string> rows)
-	{
-		ptree XMLobjectL;
-		std::string tag, value;
-
-		for (auto i : boost::combine(tags, rows))
-		{
-			//tag contains tags, value contains corresponding values
-			boost::tie(tag, value) = i;
-			XMLobjectL.put("annotation.object." + tag, value);
-		}
-		write_xml("C:\\Users\\gmano\\Desktop\\csv_xml\\" + std::to_string(ctr) + ".xml", XMLobjectL, std::locale(),
-			xml_writer_make_settings<ptree::key_type>(' ', 1u));
-		ctr++;
-	}
-
-public:
-	void CSVToXML(std::string path)
-	{
+		static int ctr;
+		static std::unordered_map<std::string, int> fileNames;
 		std::vector<std::string> tags;
 		std::vector<std::string> rows;
 		std::ifstream file(path);
-
 		std::string line;
+
 		std::getline(file, line);
 
-		tags = this->tokenize(line);
+		auto tokenize = [](std::string line)
+		{
+			std::vector<std::string> col_names;
+
+			tokenizer<escaped_list_separator<char> > tk(line, escaped_list_separator<char>());
+			for (tokenizer<escaped_list_separator<char> >::iterator i(tk.begin()); i != tk.end(); ++i)
+				col_names.push_back(*i);
+
+			return col_names;
+		};
+
+		auto create_XML = [](std::vector<std::string>& tags, std::vector<std::string> rows)
+		{
+			ptree XMLobjectL;
+			std::string tag, value;
+
+			for (auto i : boost::combine(tags, rows))
+			{
+				//tag contains tags, value contains corresponding values
+				boost::tie(tag, value) = i;
+				XMLobjectL.put("annotation.object." + tag, value);
+			}
+			write_xml("C:\\Users\\gmano\\Desktop\\csv_xml\\" + std::to_string(ctr) + ".xml", XMLobjectL, std::locale(),
+				xml_writer_make_settings<ptree::key_type>(' ', 1u));
+			ctr++;
+		};
+
+		tags = tokenize(line);
 
 		while (std::getline(file, line))
-			create_XML(tags, this->tokenize(line));
+			create_XML(tags, tokenize(line));
+	}
+
+public:
+	void csvTOxml(std::string path)
+	{
+		csvxmlHelper(path);
 	}
 };
-int Convert::ctr;
+
 
 int main() {
 	Convert foo;
-	foo.CSVToXML("C:\\Users\\gmano\\Desktop\\csv_xml\\csv.csv");
+	foo.csvTOxml("C:\\Users\\gmano\\Desktop\\csv_xml\\csv.csv");
 }
